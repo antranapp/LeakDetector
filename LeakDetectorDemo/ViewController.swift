986f0d88e07381f4ed35dd75aa5795c71dfb1490
An Tran <binhan.tran@sc.com>
2020-11-13 21:13:08 +0800

    Initial Commit

---
+++ LeakDetectorDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeakDetectorDemo
+//
+//  Created by Tran, Binh An on 13/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
