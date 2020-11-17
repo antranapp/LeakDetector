90a6cbfca963b8bd3dcfeae15c18a4d4212bcec4
An Tran <binhan.tran@sc.com>
2020-11-17 15:34:22 +0800

    Add DispatchQueue Sectioin

---
+++ LeakDetectorDemo/NoLeak/DispatchQueue/DispatchAsyncViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Copyright © 2020 An Tran. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class DispatchAsyncViewController1: UIViewController {
+
+    private let queue = DispatchQueue.main
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        queue.async {
+            self.view.tag = 111
+        }
+    }
+}
+
+class DispatchAsyncViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        DispatchQueue.main.async {
+            self.view.tag = 111
+        }
+    }
+}
